0be3ccff1e4f5257014a4d0f41e9e56db8a411ef
BraveLion0507 <154337248+BraveLion0507@users.noreply.github.com>
2023-12-22 01:43:13 -0600

    change server_path

--- src/constants/SERVER_PATH.js
+++ src/constants/SERVER_PATH.js
@@ -1,10 +1,9 @@
-const SERVER_PATH = "http://localhost:8000/"
-const CLIENT_PATH = "http://localhost:3000/"
-const SERVER_WSPATH = "ws://localhost:8000/"
+// const SERVER_PATH = "http://localhost:8000/"
+const SERVER_PATH = "https://apple-repair-fast-api-six.vercel.app/";
+// const CLIENT_PATH = "http://localhost:3000/"
+const CLIENT_PATH = "https://apple-repair-frontend-taupe.vercel.app/";
+const SERVER_WSPATH = "https://apple-repair-fast-api-six.vercel.app/";
 
-export {
-    SERVER_WSPATH,
-    CLIENT_PATH
-}
+export { SERVER_WSPATH, CLIENT_PATH };
 
-export default SERVER_PATH
+export default SERVER_PATH;
