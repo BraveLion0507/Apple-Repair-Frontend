df7c38c0b694c3a1bf75e1dc2362c2d84959c4d2
BraveLion0507 <154337248+BraveLion0507@users.noreply.github.com>
2023-12-22 03:17:42 -0600

    Modify web socket server path

--- src/constants/SERVER_PATH.js
+++ src/constants/SERVER_PATH.js
@@ -2,7 +2,8 @@
 const SERVER_PATH = "https://apple-repair-fast-api-six.vercel.app/";
 // const CLIENT_PATH = "http://localhost:3000/"
 const CLIENT_PATH = "https://apple-repair-frontend-taupe.vercel.app/";
-const SERVER_WSPATH = "https://apple-repair-fast-api-six.vercel.app/";
+// const SERVER_WSPATH = "ws://localhost:8000/";
+const SERVER_WSPATH = "ws://apple-repair-fast-api-six.vercel.app/";
 
 export { SERVER_WSPATH, CLIENT_PATH };
 
